3e6a22ec1757c216711510129b1fc6c8a3e8fc96
k1mlka <k1mlka@outlook.com>
2025-07-14 11:10:01 +0800

    子窗口 框架不同导致layerd无法添加的问题复现

--- RuhuyagayBemkaijearfear/RuhuyagayBemkaijearfear/MainWindow.xaml.cs
+++ RuhuyagayBemkaijearfear/RuhuyagayBemkaijearfear/MainWindow.xaml.cs
@@ -1,14 +1,19 @@
-﻿using System.Linq;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Vanara.PInvoke;
 
 namespace RuhuyagayBemkaijearfear
 {
@@ -20,6 +25,30 @@ namespace RuhuyagayBemkaijearfear
         public MainWindow()
         {
             InitializeComponent();
+
+            var edge = Process.Start(new ProcessStartInfo
+            {
+                FileName = "msedge",
+                UseShellExecute = true,
+            });
+
+            var handle = new WindowInteropHelper(this).EnsureHandle();
+            // 增加 Child Style
+            var style = (User32.GetWindowLong(handle, User32.WindowLongFlags.GWL_STYLE)
+                | (int)User32.WindowStyles.WS_CHILD);
+            User32.SetWindowLong(handle, User32.WindowLongFlags.GWL_STYLE, style);
+
+            // 等待宿主父窗口 edge 完全启动
+            Thread.Sleep(3000);
+            var proc = Process.GetProcessesByName("msedge").FirstOrDefault(proc => proc.MainWindowHandle != IntPtr.Zero);
+            nint edgeHandle = proc.MainWindowHandle;
+
+            // HACK：如果是 net9，那么会正常显示透明的子窗口
+            // 如果是 net48，layerd 没有被正常添加，透明显示为黑色
+            User32.SetParent(handle, edgeHandle);
+
+            User32.GetClientRect(edgeHandle, out var rectClient);
+            User32.SetWindowPos(handle, IntPtr.Zero, 0, 0, rectClient.Width, rectClient.Height, User32.SetWindowPosFlags.SWP_NOZORDER);
         }
 
         private void SetTransparentHitThroughButton_OnClick(object sender, RoutedEventArgs e)
